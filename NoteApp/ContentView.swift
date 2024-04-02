16cc575952ab6ccf325612498f46a6bdd24f7c0d
IOS <6410685140@student.tu.ac.th>
2024-04-02 14:59:49 +0700

    Initial Commit

---
+++ NoteApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NoteApp
+//
+//  Created by นายธนภัทร สาระธรรม on 2/4/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by นายธนภัทร สาระธรรม on 2/4/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
